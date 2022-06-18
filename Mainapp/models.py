94d61c140ee25cbfdbe93fd441ede5ee4efd30e3
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-06-18 16:57:46 +0300

    ft:added new models

--- Mainapp/models.py
+++ Mainapp/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User 
+from cloudinary.models import CloudinaryField
+
+class Hood(models.Model):
+    name = models.CharField(max_length=200)
+    location =  models.CharField(max_length=200)
+    img = CloudinaryField('image')
+    admin = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
+class Profile(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=300, blank=True, null=True)
+    pic = CloudinaryField('image', blank = True)
+    hood = models.ForeignKey(Hood, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.owner.username
+
+
+class News(models.Model):
+    poster = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=300, blank=True, null=True)
+    body = models.TextField()
+    hood = models.ForeignKey(Hood, on_delete=models.SET_NULL, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) :
+        return self.title
+
+class Business(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    name = models.CharField(max_length=300, blank=True, null=True)
+    location = models.TextField()
+    contact = models.CharField(max_length=300, blank=True, null=True)
+    hood = models.ForeignKey(Hood, on_delete=models.SET_NULL, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) :
+        return self.name
